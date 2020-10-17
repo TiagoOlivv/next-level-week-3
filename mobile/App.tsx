b8bf47970563582046e40df6c03b75fa16f6adc0
tiagoolivv <suubmundo@gmail.com>
2020-10-17 18:45:05 -0300

    mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
+
+import Routes from "./src/routes";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
+  return <Routes />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ mobile/src/routes.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  StackHeaderProps,
+} from "@react-navigation/stack";
+
+import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanageDetails from "./pages/OrphanageDetails";
+import OrphanageData from "./pages/CreateOrphanage/OrphanageData";
+import SelectMapPosition from "./pages/CreateOrphanage/SelectMapPosition";
+import Header from "./components/Header";
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: "#f2f3f5" },
+        }}
+      >
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen
+          name="OrphanageDetails"
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />,
+          }}
+        />
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />,
+          }}
+        />
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />,
+          }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
+};
